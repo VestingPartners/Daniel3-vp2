849bd21be90296a328587069d53856a0ce11dfe5
Daniel-jsx <167225409+VestingPartners@users.noreply.github.com>
2024-07-23 00:00:14 -0400

    port separado de server

--- app/api/db.js
+++ app/api/db.js
@@ -5,12 +5,14 @@ const apiSecret_server = process.env.SERVER_DANIEL;
 const apiSecret_database = process.env.DATABASE_DANIEL;
 const apiSecret_user = process.env.USER_DANIEL;
 const apiSecret_password = process.env.PASSWORD_DANIEL;
+const apiSecret_port = process.env.PORT_DANIEL;
 
 const dbConfig = {
     user: apiSecret_user,
     password: apiSecret_password,
     database: apiSecret_database,
     server: apiSecret_server,
+    port: apiSecret_port,
     pool: {
         max: 10,
         min: 0,
@@ -21,7 +23,6 @@ const dbConfig = {
         trustServerCertificate: false // change to true for local dev / self-signed certs
     }
 }
-
 export default async function handler(req, res) {
     let pool
     try {
